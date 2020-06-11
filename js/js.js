4fa91b4d6836b4261d2214306d583e233e0258ca
Valentin404 <valik49717@gmail.com>
2020-06-12 02:27:45 +0300

    4

--- js/js.js
+++ js/js.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
  $('.header__burger').click(function(event) {
-    //  При гажатии на иконку добавляется класс active,  пр повторном убирается
      $('.header__burger, .header__menu').toggleClass('active');
-    //  Убираем скрул при открытом меню
     $('body').toggleClass('lock');
  });
 });
+$(document).ready(function() {
+   $('.basket').click(function(event) {
+       $('.basket, .modal-dialog').toggleClass('active');
+   });
+  });
